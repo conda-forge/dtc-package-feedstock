a348737f4ac0dd3b38d98c654d310277ada97420
Brandon Maier <brandon.maier@gmail.com>
2024-11-17 16:50:17 -0600

    migrate python install to pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,11 +7,11 @@ if [[ "$(uname -s)" = "Darwin" ]]; then
     # Tests don't build on osx due to GCC-specific assembly directives in
     # tests/trees.S So, we only do minimal existance and import testing on the
     # osx package
-    meson setup ${MESON_ARGS} -D python=enabled -D tests=false build
+    meson setup ${MESON_ARGS} -D python=disabled -D tests=false build
 else
     # Temporarily disable tests until build errors in newer GCC can be fixed
     # meson setup ${MESON_ARGS} -D python=enabled build
-    meson setup ${MESON_ARGS} -D python=enabled -D tests=false build
+    meson setup ${MESON_ARGS} -D python=disabled -D tests=false build
 fi
 
 meson compile -C build
