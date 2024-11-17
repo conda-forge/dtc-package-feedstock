dd357f17608f32c567f02c92320e8cdf00163e1b
Brandon Maier <brandon.maier@gmail.com>
2024-11-17 11:49:32 -0600

    add MESON_ARGS to setup

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,9 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
     # make check doesn't build on osx due to GCC-specific assembly directives
     # in tests/trees.S So, we only do minimal existance and import testing on
     # the osx package
-    meson setup -D python=enabled -D tests=false build
+    meson setup ${MESON_ARGS} -D python=enabled -D tests=false build
 else
-    meson setup -D python=enabled build
+    meson setup ${MESON_ARGS} -D python=enabled build
 fi
 
 meson compile -C build
