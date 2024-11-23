5d7db31e70f1265b7634afaf1dc82e0fd032124a
Brandon Maier <brandon.maier@gmail.com>
2024-11-23 13:52:30 -0600

    remove old build comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,6 @@ if [[ "$(uname -s)" = "Darwin" ]]; then
     # osx package
     meson setup ${MESON_ARGS} -D python=disabled -D tests=false build
 else
-    # Temporarily disable tests until build errors in newer GCC can be fixed
-    # meson setup ${MESON_ARGS} -D python=enabled build
     meson setup ${MESON_ARGS} -D python=disabled -D tests=true build
 fi
 
