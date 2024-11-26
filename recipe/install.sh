ae94eb367c903add2c2fa6e9996829742845f867
Brandon Maier <brandon.maier@gmail.com>
2024-11-26 11:49:24 -0600

    skip rebuild during install step

--- recipe/install.sh
+++ recipe/install.sh
@@ -4,10 +4,10 @@ set -xe
 
 case "$PKG_NAME" in
 dtc)
-	meson install -C build
+	meson install --no-rebuild -C build
 	;;
 libfdt)
-	meson install -C build
+	meson install --no-rebuild -C build
 	;;
 *)
 	echo "::ERROR:: unknown PKG_NAME: '$PKG_NAME'" >&2
