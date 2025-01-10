6a8a34c4aee08546ddcc4f4f5e45c3b00312e695
Brandon Maier <brandon.maier@gmail.com>
2025-01-09 21:34:00 -0600

    run build tests when cross-compiling with emulator

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,15 @@
 set -e
 set -x
 
-if  [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" || "$(uname -s)" = "Darwin" ]]; then
+if [[ "$(uname -s)" = "Darwin" ]]; then
     # Tests don't build on osx due to GCC-specific assembly directives in
     # tests/trees.S So, we only do minimal existance and import testing on the
     # osx package
     tests=false
-else
+elif [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
     tests=true
+else
+    tests=false
 fi
 
 meson setup ${MESON_ARGS} -D python=disabled -D tests=$tests build
